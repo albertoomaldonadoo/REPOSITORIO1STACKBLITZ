06621844fb9728fea0043e1d99d592cb34ec5207
Alberto Maldonado Triana <amaltri0308@cpifpalanturing.es>
2025-10-02 10:17:40 +0200

    primer commit

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,100 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { fromFetch } from 'rxjs/fetch';
+import { switchMap, of, catchError } from 'rxjs';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+      <h1>Personajes de Dragon Ball</h1>
+      <div id="mostrarPersonajes">
+        
+      </div>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+async function listaDePersonajes() {
+    const respuestaFetch = await fetch('https://dragonball-api.com/api/characters');
+    const datosPersonajes = await respuestaFetch.json();
+    const listaPersonajes = datosPersonajes.items;
+
+    const contenedor = document.getElementById('mostrarPersonajes');
+
+    listaPersonajes.forEach((personaje: any) => {
+      const tarjeta = document.createElement('div');
+      tarjeta.innerHTML = 
+      `
+        <h2>Nombre: ${personaje.name}</h2>
+        <p>ID: ${personaje.id}</p>
+        <p>KI: ${personaje.ki}</p>
+        <p>MaxKi: ${personaje.maxKi}</p>
+        <p>Raza: ${personaje.race}</p>
+        <p>Genero: ${personaje.gender}</p>
+        <p>Descripcion: ${personaje.description}</p>
+        <p>Afilacion: ${personaje.affiliation}</p>
+        
+        <img src="${personaje.image}" alt="fotodelpersonaje"/>
+      `;
+
+      contenedor!.appendChild(tarjeta);
+    });
+}
+
+listaDePersonajes();
+
+/*CON RXJS*/
+
+const respuestaFetchRXJS = fromFetch('https://dragonball-api.com/api/characters').pipe(
+  switchMap(respuestaFetchRXJS => {
+    if (respuestaFetchRXJS.ok) {
+      return respuestaFetchRXJS.json();
+    } else {
+      return of({ error: true, message: `Error ${ respuestaFetchRXJS.status }` });
+    }
+  })
+);
+ 
+respuestaFetchRXJS.subscribe({
+    next: (result:any) => {
+      const element = document.getElementById('app');
+      if(element){
+        result['items'].map((character:any)=>{
+          element.innerHTML+=`
+            <div class="card">
+
+              <div class="data-container">
+                 <h2>ID: ${character['name']}</h2>
+              </diV>
+
+              <div class="image-container">
+                 <img src="${character['image']}">
+              </div>
+
+              <div class="data-container">
+                 <p>ID: ${character['id']}</p>
+              </diV>
+
+              <div class="data-container">
+                 <p>Descripcion: ${character['description']}</p>
+              </diV>
+
+              <div class="data-container">
+                 <p>KI: ${character['ki']}</p>
+              </diV>
+
+              <div class="data-container">
+                 <p>MaxKI: ${character['maxKi']}</p>
+              </diV>
+
+              <div class="data-container">
+                 <p>Raza: ${character['race']}</p>
+              </diV>
+
+              <div class="data-container">
+                 <p>Afilacion: ${character['affiliation']}</p>
+              </diV>
+            </div>\n`
+        })
+      }
+      
+    },
+    complete: () => console.log('done'),
+});
